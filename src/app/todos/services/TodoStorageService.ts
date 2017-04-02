cf9b3531cdfd8fee0bde638e554f94607e3ee200
Yves Schelpe <yves.schelpe@gmail.com>
2017-04-02 10:54:41 +0200

    cleaned up TodoStorageService code for firebase

--- src/app/todos/services/TodoStorageService.ts
+++ src/app/todos/services/TodoStorageService.ts
@@ -3,30 +3,14 @@ import * as firebase from 'firebase';
 
 export class TodoStorageService {
 
-    /*todoItems: TodoItem[] = [
-        new TodoItem('Todo Item 1', false),
-        new TodoItem('Todo Item 2', true),
-        new TodoItem('Todo Item 3', true),
-        new TodoItem('Todo Item 4', false)
-    ];*/
-
-    constructor(private $q: angular.IQService, private $firebaseArray: any) { }
+    constructor(private $firebaseArray: any) { }
 
     getAll(): angular.IPromise<TodoItem[]> {
         const ref = firebase.database().ref().child('todos');
-        /*const todos =*/ return this.$firebaseArray(ref)
-            .$loaded();
-        /*.then((todosArray: any) => {
-            this.todoItems = [];
-            for (let todo of todosArray) {
-                this.todoItems.push(new TodoItem(todo.title, todo.completed));
-            }
-            console.log(this.todoItems);
-        });*/
-        /*return this.$q.resolve(this.todoItems);*/
+        return this.$firebaseArray(ref).$loaded();
     }
 
     add(todoItem: TodoItem) {
-        //this.todoItems.push(todoItem);
+        // this.todoItems.push(todoItem);
     }
 }
