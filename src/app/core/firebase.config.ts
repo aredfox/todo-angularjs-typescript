436f247c9069bb6bd83f22ca494874e8a2d24e2b
Yves Schelpe <yves.schelpe@gmail.com>
2017-04-11 16:38:06 +0200

    moved firebase configuration to it's own file

---
+++ src/app/core/firebase.config.ts
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBb9nUw2-EuOFiRDlmCGKyBbUkYWDNr30E',
+    authDomain: 'todosth-876e4.firebaseapp.com',
+    databaseURL: 'https://todosth-876e4.firebaseio.com',
+    projectId: 'todosth-876e4',
+    storageBucket: 'todosth-876e4.appspot.com',
+    messagingSenderId: '777328246324'
+};
+
+firebase.initializeApp(firebaseConfig);
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
  */
 import './polyfills';
 import './vendor';
+import './app/core/firebase.config';
 
 /**
  * Temporary Import angular
@@ -14,20 +15,6 @@ import * as angular from 'angular';
  *  Import module to be bootstrapped
  */
 import { moduleName as appModule } from './app/app.module';
-
-/**
- * Bootstrap firebase
- */
-import * as firebase from 'firebase';
-const firebaseConfig = {
-  apiKey: 'AIzaSyBb9nUw2-EuOFiRDlmCGKyBbUkYWDNr30E',
-  authDomain: 'todosth-876e4.firebaseapp.com',
-  databaseURL: 'https://todosth-876e4.firebaseio.com',
-  projectId: 'todosth-876e4',
-  storageBucket: 'todosth-876e4.appspot.com',
-  messagingSenderId: '777328246324'
-};
-firebase.initializeApp(firebaseConfig);
 
 /**
  * Bootstrap the application using the imported moduleName
